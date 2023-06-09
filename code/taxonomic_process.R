15f512621b27517a9f20b01b452f5c014018fb46
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2023-06-09 13:43:23 -0300

    revised code that is more streamlined.

--- code/taxonomic_process.R
+++ code/taxonomic_process.R
@@ -101,27 +101,33 @@ id_fun <- function(x,tax_order,return="id"){ #extract the lowest id key
         
         ocean_ord <- c("Atlantic","Arctic","Pacific")
 
-        #create bounding boxes that can be used by r-obis 
-        bb_atlantic <- bioregions%>%
-              filter(REGION == "Atlantic")%>%
-              st_bbox()%>%
-              st_as_sfc()%>%
-              st_transform(latlong)%>%
-              st_as_text()
-        
-        bb_arctic <- bioregions%>%
-              filter(REGION == "Arctic")%>%
-              st_bbox()%>%
-              st_as_sfc()%>%
-              st_transform(latlong)%>%
-              st_as_text()
-        
-        bb_pacific <- bioregions%>%
-              filter(REGION == "Pacific")%>%
-              st_bbox()%>%
-              st_as_sfc()%>%
-              st_transform(latlong)%>%
-              st_as_text()
+        #create bounding boxes that can be used by r-obis with a 1000 km boundary.  
+    bb_atlantic <- bioregions%>%
+                  filter(REGION == "Atlantic")%>%
+                  st_bbox()%>%
+                  st_as_sfc()%>%
+                  st_transform(CanProj)%>% #area based projection (units m)
+                  st_buffer(1000 * 1000)%>% # 1000 km buffer. 
+                  st_transform(latlong)%>%
+                  st_as_text()
+    
+    bb_arctic <- bioregions%>%
+                  filter(REGION == "Arctic")%>%
+                  st_bbox()%>%
+                  st_as_sfc()%>%
+                  st_transform(CanProj)%>% #area based projection (units m)
+                  st_buffer(1000 * 1000)%>% # 1000 km buffer
+                  st_transform(latlong)%>%
+                  st_as_text()
+    
+    bb_pacific <- bioregions%>%
+                  filter(REGION == "Pacific")%>%
+                  st_bbox()%>%
+                  st_as_sfc()%>%
+                  st_transform(CanProj)%>% #area based projection (units m)
+                  st_buffer(1000 * 1000)%>% # 1000 km buffer
+                  st_transform(latlong)%>%
+                  st_as_text()
         
         bb_zones <- list(bb_atlantic,bb_arctic,bb_pacific)
         names(bb_zones) <- ocean_ord
@@ -139,11 +145,17 @@ id_fun <- function(x,tax_order,return="id"){ #extract the lowest id key
         
         #for each species check the occurrence records for each region
         
-        sp_names <- worms_df$species
+        ocean_intersection <- list()
+        
+        sp_names <- worms_df%>%
+                    filter(env!="Freshwater")%>% #do not search for the freshwater species. 
+                    pull(species)
+        
+        eez_thresh = 250 #distance in km for an obis observation to be associated 
         
         for(i in sp_names){
           
-          message(paste0("working on "),i) #progress message 
+          message(paste0("working on "),i," ",which(i == sp_names),"/",length(sp_names)) #progress message 
           
           sp_temp <- NULL
           
@@ -164,220 +176,69 @@ id_fun <- function(x,tax_order,return="id"){ #extract the lowest id key
             rm(temp)
             
           } #end 'j' loop
-        } #end 'i' loop
-        
-        #narrow down to within the EEZ and within a threshold of the EEZ
-        
-        eez_thresh = 250 #distance in km for an obis observation to be associated 
-        
-        robis_names <- dir("output/robis_records/",full.names = TRUE) #these are the species within the bounding boxes 
-        
-      ocean_intersection <- list()
-      null_extracts <- NULL
-        
-        for (i in robis_names){
-          
-          message(paste0("Working on ",gsub("output/robis_records/robis_","",i)," ",which(i == robis_names),"/",length(robis_names)))
-          
-          load(i) #load the extractions from previous loop
           
           if(!is.null(sp_temp)){
-         
+            
+          #format data as a spatial data.frame
           sf_format <- sp_temp%>%
-                       dplyr::select(ocean,decimalLatitude,decimalLongitude)%>%
-                       st_as_sf(coords=c("decimalLongitude","decimalLatitude"),crs=latlong)%>%
-                       mutate(ocean = ocean_ord[as.numeric(st_intersects(.,oceans))])
-        
-            #if no points fall within the ocean find out which ocean is the closest and how close
-            if(sum(is.na(sf_format$ocean)) == nrow(sf_format)){
-              
-              ocean_dist <- sf_format%>%
-                mutate(ocean = ocean_ord[st_nearest_feature(.,oceans)])
-              
-              for(j in 1:nrow(ocean_dist)){
+                      dplyr::select(ocean,decimalLatitude,decimalLongitude)%>%
+                      st_as_sf(coords=c("decimalLongitude","decimalLatitude"),crs=latlong)%>%
+                      mutate(ocean = ocean_ord[as.numeric(st_intersects(.,oceans))])
+          
+          #if no points fall within a 'Canadian' ocean check which is the nearest (as the crow would fly)
+          if(sum(is.na(sf_format$ocean)) == nrow(sf_format)){
+            
+            ocean_dist <- sf_format%>%
+                          mutate(ocean = ocean_ord[st_nearest_feature(.,oceans)])
+            
+            #now get the distance from each record to it's (Canadian) nearest ocean polygon
+            for(j in 1:nrow(ocean_dist)){
               
               ocean_dist[j,"dist"] <- st_distance(ocean_dist[j,],oceans%>%
-                                                                 filter(REGION == ocean_dist[j,]%>%pull(ocean)))%>%as.numeric()/1000
-
-              }
+                                                    filter(REGION == ocean_dist[j,]%>%pull(ocean)))%>%as.numeric()/1000
               
-              ocean_dist <- ocean_dist%>%distinct(dist,.keep_all=TRUE) #get rid of any duplicate distances. 
-              
-              #if all distances are further than the limit only include the closest one
-              if(min(ocean_dist$dist)>eez_thresh){ocean_dist <- ocean_dist%>%filter(dist==min(dist,na.rm=T))}else{ocean_dist <- ocean_dist%>%filter(dist<=eez_thresh)}
-              
-              out <- data.frame(species=unique(sp_temp$species),
-                                ocean = ocean_dist%>%pull(ocean)%>%unique(),
-                                dist = min(ocean_dist$dist)) #closest observation
-              
-            } #ocean distance check
+            }
+            
+            #get rid of any duplicate distances. 
+            ocean_dist <- ocean_dist%>%distinct(dist,.keep_all=TRUE) 
+            
+            #if all distances are further than the limit only include the closest one
+            if(min(ocean_dist$dist)>eez_thresh){ocean_dist <- ocean_dist%>%
+                                                              filter(dist==min(dist,na.rm=T))}else{ocean_dist <- ocean_dist%>%filter(dist<=eez_thresh)}
+            
+            out <- data.frame(species=i,
+                   ocean = ocean_dist%>%pull(ocean)%>%unique()%>%paste(.,collapse="-"),
+                   dist = min(ocean_dist$dist)) #closest observation
+            
+          }else{
+            
+            out <- data.frame(species=i,
+                              ocean=sf_format%>%filter(!is.na(ocean))%>%pull(ocean)%>%unique()%>%paste(.,collapse="-"),
+                              dist=0)
+            
+          }
           
-            #if there are observations from within
-              if(sum(is.na(sf_format$ocean)) != nrow(sf_format)){
-              
-                  out <- data.frame(species=unique(sp_temp$species),
-                                  ocean = sf_format%>%filter(!is.na(ocean))%>%pull(ocean)%>%unique(),
-                                  dist=0)
-                
-              } #0 demarking 'within' while keeping it numeric 
+          }#end if(!is.null(sp_temp))
           
-          ocean_intersection[[unique(sp_temp$species)]] <- out
+          #no observations within 1000 km of Canadian oceans 
+          if(is.null(sp_temp)){
+            
+            out=data.frame(species=i,
+                           ocean=NA,
+                           dist=NA)
+          }
           
-          } #null check 
+          ocean_intersection[[i]] <- out
           
-          if(is.null(sp_temp)){null_extracts <- c(null_extracts,i)}
+          rm(out)
           
-          rm(sp_temp)
-          
-    }  #ocean 'i' intersection for each obis extraction 
+        } #end 'i' loop
       
 #save the interium outputs
-save(ocean_intersection,file="output/ocean_intersection.RData")
-save(null_extracts,file="output/null_extracts.RData")      
+save(ocean_intersection,file="output/ocean_intersection.RData")   
 
 #bind the output together
 ocean_df <- do.call("rbind",ocean_intersection)%>%
-  arrange(species,ocean)
-
-
-table(ocean_df$ocean)
-
-
-
-#Do re_extractions at a larger extent for the null_extracts ----------
-
-bb_atlantic_lg <- bioregions%>%
-                  filter(REGION == "Atlantic")%>%
-                  st_bbox()%>%
-                  st_as_sfc()%>%
-                  st_transform(CanProj)%>% #area based projection (units m)
-                  st_buffer(1000 * 1000)%>% # 1000 km buffer. 
-                  st_transform(latlong)%>%
-                  st_as_text()
-
-bb_arctic_lg <- bioregions%>%
-                filter(REGION == "Arctic")%>%
-                st_bbox()%>%
-                st_as_sfc()%>%
-                st_transform(CanProj)%>% #area based projection (units m)
-                st_buffer(1000 * 1000)%>% # 1000 km buffer
-                st_transform(latlong)%>%
-                st_as_text()
-
-bb_pacific_lg <- bioregions%>%
-                  filter(REGION == "Pacific")%>%
-                  st_bbox()%>%
-                  st_as_sfc()%>%
-                  st_transform(CanProj)%>% #area based projection (units m)
-                  st_buffer(1000 * 1000)%>% # 1000 km buffer
-                  st_transform(latlong)%>%
-                  st_as_text()
-
-bb_zones_lg <- list(bb_atlantic_lg,bb_arctic_lg,bb_pacific_lg)
-names(bb_zones_lg) <- ocean_ord
-
-null_sp_names <- null_extracts%>%
-            gsub("output/robis_records/robis_","",.)%>%
-            gsub(".RData","",.)%>%
-            gsub("_"," ",.)
-
-
-for(i in null_sp_names){
- 
-  message(paste0("working on "),i) #progress message 
-  
-  sp_temp <- NULL
-  
-  for(j in 1:3){
-    
-    message(paste0(names(bb_zones_lg)[j])," record search ..")  
-    
-    temp <- occurrence(i,geometry = bb_zones_lg[j])
-    
-    if(length(temp) !=0){sp_temp <- rbind(sp_temp,temp%>%
-                                            mutate(ocean = (names(bb_zones_lg)[j]))%>%
-                                            dplyr::select(scientificName,species,
-                                                          decimalLatitude,decimalLongitude,ocean))}
-    
-    #save interim outputs
-    save(sp_temp,file=paste0("output/robis_records/lgextent/robis_",gsub(" ","_",i),".RData"))
-    
-    rm(temp)
-    
-  } #end 'j' loop
-} #end 'i' loop
-
-
-eez_thresh_lg = 1000 #distance in km for an obis observation to be associated 
-
-robis_names_lg <- dir("output/robis_records/lgextent/",full.names = TRUE) #these are the species within the bounding boxes 
-
-ocean_intersection_lg <- list()
-null_extracts_lg <- NULL
-
-for (i in robis_names_lg){
-  
-  message(paste0("Working on ",gsub("output/robis_records/lgextent/robis_","",i)," ",which(i == robis_names_lg),"/",length(robis_names_lg)))
-  
-  load(i) #load the extractions from previous loop
-  
-  if(!is.null(sp_temp)){
-    
-    sf_format <- sp_temp%>%
-      dplyr::select(ocean,decimalLatitude,decimalLongitude)%>%
-      st_as_sf(coords=c("decimalLongitude","decimalLatitude"),crs=latlong)%>%
-      mutate(ocean = ocean_ord[as.numeric(st_intersects(.,oceans))])
-    
-    #if no points fall within the ocean find out which ocean is the closest and how close
-    if(sum(is.na(sf_format$ocean)) == nrow(sf_format)){
-      
-      ocean_dist <- sf_format%>%
-        mutate(ocean = ocean_ord[st_nearest_feature(.,oceans)])
-      
-      for(j in 1:nrow(ocean_dist)){
-        
-        ocean_dist[j,"dist"] <- st_distance(ocean_dist[j,],oceans%>%
-                                              filter(REGION == ocean_dist[j,]%>%pull(ocean)))%>%as.numeric()/1000
-        
-      }
-      
-      ocean_dist <- ocean_dist%>%distinct(dist,.keep_all=TRUE) #get rid of any duplicate distances. 
-      
-      #if all distances are further than the limit only include the closest one
-      if(min(ocean_dist$dist)>eez_thresh){ocean_dist <- ocean_dist%>%filter(dist==min(dist,na.rm=T))}else{ocean_dist <- ocean_dist%>%filter(dist<=eez_thresh)}
-      
-      out <- data.frame(species=unique(sp_temp$species),
-                        ocean = ocean_dist%>%pull(ocean)%>%unique(),
-                        dist = min(ocean_dist$dist)) #closest observation
-      
-    } #ocean distance check
-    
-    #if there are observations from within
-    if(sum(is.na(sf_format$ocean)) != nrow(sf_format)){
-      
-      out <- data.frame(species=unique(sp_temp$species),
-                        ocean = sf_format%>%filter(!is.na(ocean))%>%pull(ocean)%>%unique(),
-                        dist=0)
-      
-    } #0 demarking 'within' while keeping it numeric 
-    
-    ocean_intersection_lg[[unique(sp_temp$species)]] <- out
-    
-  } #null check 
-  
-  if(is.null(sp_temp)){null_extracts_lg <- c(null_extracts_lg,i)}
-  
-  rm(sp_temp)
-  
-}  #ocean 'i' intersection for each obis extraction 
-        
-#save the interim output
-save(ocean_intersection_lg,file="output/ocean_intersection_lg.RData")
-       
-#Combine the ocean intersections at the localized and large extents
-ocean_df <- do.call("rbind",ocean_intersection_lg)%>%
-            rbind(.,do.call("rbind",ocean_intersection))%>%
             arrange(species,ocean)%>%
-            left_join(.,sp_fishbase)
-
+            left_join(.,worms_df)
 
