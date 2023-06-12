2dfc477b8738ce6d054ad0304e726b3d741a85e3
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2023-06-12 11:50:25 -0300

    updated outputs summarizing species distributions.

--- code/taxonomic_summary.R
+++ code/taxonomic_summary.R
@@ -8,97 +8,121 @@ library(viridis)
 library(sf)
 library(rnaturalearth)
 library(eulerr)
+library(gridExtra)
+
+sf_use_s2(FALSE)
 
 #load data from taxonomic_process.R
-load("output/ocean_intersection.RData")
-load("output/ocean_intersection_lg.RData")
-load("output/sp_fishbase.RData")
+load("output/ocean_df.RData")
 load("output/taxonomy_wide.RData")
 
-#plot order
+#projections -----------
+latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+utm <- "+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs +ellps=GRS80 +towgs84=0,0,0" #works for the Scotian Shelf
+CanProj <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=63.390675 +lon_0=-91.86666666666666 +x_0=6200000 +y_0=3000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
+#plot order --------
 ocean_ord <- c("Atlantic","Arctic","Pacific")
 
-#compile a dataframe 
-ocean_df1 <- do.call("rbind",ocean_intersection_lg)%>%
-            rbind(.,do.call("rbind",ocean_intersection))%>%
-            arrange(species,ocean)%>%
-            left_join(.,sp_fishbase)
+#polygons ----------
+bioregions <- read_sf("data/shapefiles/DFO_Marine_Bioregions_Clipped_1M_CAEAC_2012_05_31.shp")%>%
+              st_transform(CanProj)
 
-#fix subspecies
-ocean_df2 <- rbind(ocean_df1,
-                   ocean_df1[ocean_df1$species == "Esox americanus",], #two subspecies for Exox americanus
-                   ocean_df1[ocean_df1$species == "Oncorhynchus clarkii",], #three subspecies for Onorhynchus clarkii
-                   ocean_df1[ocean_df1$species == "Oncorhynchus clarkii",])
+oceans <- bioregions%>%
+          filter(REGION %in% ocean_ord)%>%
+          group_by(REGION)%>%
+          summarise(geometry=st_union(geometry))%>%
+          ungroup()%>%
+          st_as_sf()%>%
+          st_transform(CanProj)%>%
+          mutate(region=factor(REGION,levels=ocean_ord))%>% #sort
+          arrange(region)%>%
+          dplyr::select(-region)
 
-#add in the subspecies
-ocean_df2$subspecies <- NA
+can_bounding <- bioregions%>%st_bbox()
 
-ocean_df2[ocean_df2$species=="Esox americanus","subspecies"] <- c("Esox americanus americanus","Esox americanus vermiculatus")
-ocean_df2[ocean_df2$species=="Oncorhynchus clarkii","subspecies"] <- c("Oncorhynchus clarkii bouvieri","Oncorhynchus clarkii clarkii","Oncorhynchus clarkii lewisi")
+can_bounding_polygon <- can_bounding%>%st_as_sfc()%>%st_as_sf() #convert to an sf polygon
 
-#clean it up
-out_df <- out_df%>%
-  mutate(spp = case_when(is.na(subspecies)~species,
-                         !is.na(subspecies)~subspecies))
+ggplot()+geom_sf(data=oceans)+geom_sf(data=can_bounding_polygon,fill=NA)
 
-#the process did pick up some duplicates with on 'itis' and one 'gbif' registry. 
-dup_sp <- out_df%>%
-  pull(spp)%>%
-  table()%>%
-  data.frame()%>%
-  arrange(-Freq)%>%
-  rename(spp=1)%>%
-  filter(Freq>1)%>%
-  pull(spp)%>%
-  as.character()
+#create a basemap for plotting MPAs
+basemap <- rbind(ne_states(country = "Canada",returnclass = "sf")%>%
+                   dplyr::select(name_en,geometry)%>%
+                   st_as_sf()%>%
+                   st_union()%>%
+                   st_transform(latlong)%>%
+                   st_as_sf()%>%
+                   mutate(country="Canada"),
+                 ne_states(country = "United States of America",returnclass = "sf")%>%
+                   dplyr::select(name_en,geometry)%>%
+                   st_as_sf()%>%
+                   st_union()%>%
+                   st_transform(latlong)%>%
+                   st_as_sf()%>%
+                   mutate(country="USA"),
+                 ne_states(country = "Greenland",returnclass = "sf")%>%
+                   dplyr::select(name_en,geometry)%>%
+                   st_as_sf()%>%
+                   st_union()%>%
+                   st_transform(latlong)%>%
+                   st_as_sf()%>%
+                   mutate(country="USA"))%>%
+  st_union()%>%
+  st_as_sf()%>%
+  st_transform(CanProj)%>%
+  st_intersection(.,can_bounding_polygon)
 
-tax_df <- out_df%>%
-  filter(!spp %in% dup_sp | (spp %in% dup_sp & db=="itis")) #keep the itis data for the duplicated species.
+Canada <-  ne_states(country = "Canada",returnclass = "sf")%>%
+            dplyr::select(name_en,geometry)%>%
+            st_as_sf()%>%
+            st_union()%>%
+            st_transform(CanProj)%>%
+            st_as_sf()%>%
+            mutate(country="Canada")
 
-ocean_df <- ocean_df2%>%
-  mutate(spp = case_when(is.na(subspecies)~species,
-                         !is.na(subspecies)~subspecies))%>%
-  dplyr::select(spp,ocean,dist)%>%
-  left_join(.,tax_df,by="spp")
+#Data for venn diagram -----
 
-ocean_table <- table(ocean_df$spp,ocean_df$ocean)%>%
+ocean_table1 <- ocean_df%>%
+               mutate(Atlantic = ifelse(grepl("Atlantic",ocean),TRUE,FALSE),
+                      Arctic = ifelse(grepl("Arctic",ocean),TRUE,FALSE),
+                      Pacific = ifelse(grepl("Pacific",ocean),TRUE,FALSE),
+                      Freshwater = ifelse(env == "Freshwater",TRUE,FALSE))%>%
+               dplyr::select(species,Atlantic,Arctic,Pacific,Freshwater)%>%
+               gather(key='Ocean',value='logic',Atlantic:Freshwater)%>%
+               filter(logic)%>%
+               data.frame()
+
+ocean_table <- table(ocean_table1$species,ocean_table1$Ocean)%>%
                data.frame()%>%
-               mutate(Freq=as.logical(Freq))%>%
-               spread(key=Var2,value=Freq)%>%
-               dplyr::select(-Var1)
+                mutate(Freq=as.logical(Freq))%>%
+                spread(key=Var2,value=Freq)%>%
+                dplyr::select(-Var1)
 
-png(filename = "inst/plot_venn.png",height=6,width=6,units="in",res=300)               
-  plot(euler(ocean_table, shape = "ellipse"), quantities = TRUE,fills = c("lightskyblue", "royalblue4", "springgreen4"))
+
+#make the venn plot
+set.seed(23) #so you get the same bubble orientation each time. 
+
+venn.plot <- plot(euler(ocean_table, shape = "ellipse"), quantities = TRUE,fills = c("lightskyblue", "royalblue4", "forestgreen","seagreen2"))
+
+png(filename = "inst/plot_venn.png",height=6,width=6,units="in",res=300) 
+venn.plot 
 dev.off()
 
-ocean_table2 <- ocean_df%>%
-                mutate(ocean=factor(ocean,levels=ocean_ord))%>%
-                arrange(ocean)%>%
-                group_by(spp)%>%
-                summarise(ocean=paste(ocean,collapse="-"))%>%
-                ungroup()%>%
-                data.frame()%>%
-                pull(ocean)%>%
-                table()%>%
-                data.frame()%>%
-                rename(ocean=1,count=2)%>%
-                arrange(-count)
+## Companion map
 
-ocean_table3 <- ocean_df%>%
-                group_by(ocean)%>%
-                summarise(count=n())%>%
-                ungroup()%>%
-                data.frame()%>%
-                mutate(ocean=factor(ocean,levels=ocean_ord))%>%
-                arrange(ocean)
+p1 <- ggplot()+
+  geom_sf(data=basemap,fill="white")+
+  geom_sf(data=Canada,fill="forestgreen")+
+  geom_sf(data=oceans,aes(fill=REGION))+
+  scale_fill_manual(values=c("lightskyblue", "royalblue4", "seagreen2"))+
+  theme_bw()+
+  coord_sf(expand=0,xlim=can_bounding[c(1,3)],ylim=can_bounding[c(2,4)])+
+  theme(legend.position="none")
 
-draw.triple.venn(area1=ocean_table3%>%filter(ocean=="Atlantic")%>%pull(count),
-                 area2=ocean_table3%>%filter(ocean=="Arctic")%>%pull(count),
-                 area3=ocean_table3%>%filter(ocean=="Pacific")%>%pull(count),
-                 n12=ocean_table2%>%filter(ocean=="Atlantic-Arctic")%>%pull(count)-ocean_table2%>%filter(ocean=="Atlantic-Arctic-Pacific")%>%pull(count),
-                 n23=ocean_table2%>%filter(ocean=="Arctic-Pacific")%>%pull(count)-ocean_table2%>%filter(ocean=="Atlantic-Arctic-Pacific")%>%pull(count),
-                 n13=ocean_table2%>%filter(ocean=="Atlantic-Pacific")%>%pull(count)-ocean_table2%>%filter(ocean=="Atlantic-Arctic-Pacific")%>%pull(count),
-                 n123=ocean_table2%>%filter(ocean=="Atlantic-Arctic-Pacific")%>%pull(count))
+ggsave("inst/canada_map.png",p1,height=5,width=6,units="in",dpi=300)
 
-n123=ocean_table2%>%filter(ocean=="Atlantic-Arctic-Pacific")%>%pull(count)
+#combination for github readme
+png(filename = "inst/combination.png",height=12,width=6,units="in",res=300) 
+ grid.arrange(test,p1,nrow=2)
+dev.off()
   
